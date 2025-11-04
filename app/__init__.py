e905cfda33a67f11e3ed6bbc5f4ff2979ae68a6e
davisf226 <daviragnar226@gmail.com>
2025-11-03 22:26:11 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
 from app.db import db
 from flask_mail import Mail, Message
+from flask_mail import Mail
 import secrets
 
 
+load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = 'CHAVESECRETA'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'seu-email@gmail.com'  # Substitua pelo seu e-mail
-app.config['MAIL_PASSWORD'] = 'sua-senha'  # Substitua pela sua senha
-app.config['MAIL_DEFAULT_SENDER'] = 'seu-email@gmail.com'  # O e-mail do remetente
-
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') 
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') 
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER') 
 mail = Mail(app)
 db.init_app(app)
 
